b3de5c6fcdd807751ce302d454bea56624be3f0b
Shabbir Hasan <shabbir.hasan@outlook.in>
2022-07-06 17:01:51 +0530

    Updated Master_Script.json

---
+++ Get_Master_Script.ps1
@@ -0,0 +1,8 @@
+nvs use lts
+node Get_Master_Script.js
+git add .
+git commit -a -m "Updated Master_Script.json"
+git push
+git pull
+write-host -nonewline "Program Finished, Press Enter To Exit........."
+exit
